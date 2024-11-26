28732b04aec0f84291717c4742227883246316f8
Vijay Reddy <muchukotavijay@gmail.com>
2024-11-27 01:58:44 +1100

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -5,8 +5,7 @@ async function parseExpression(input) {
   const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
   try {
     parser.feed(input);
-    const ast = parser.results[0];
-    return ast;
+    return parser.results[0];
   } catch (error) {
     const location = error.offset || input.length;
     throw new Error(`Error at character ${location}: ${error.message}`);
